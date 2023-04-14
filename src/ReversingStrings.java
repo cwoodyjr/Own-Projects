411a926416a5202551a69ee2d2aab98bf6857b3e
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2023-04-14 21:48:15 +0100

    Reversing Strings programme that takes in a word or string and prints it in reverse. added reverse word, reverse string and reverse word and strimg

--- src/ReversingStrings.java
+++ src/ReversingStrings.java
@@ -3,15 +3,41 @@ import java.util.Scanner;
 public class ReversingStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        wordReverse("reverse this string");
+        System.out.println();
+        stringReverse("reverse this string");
+        System.out.println();
+        stringWordReverse("reverse this string");
 
-        System.out.println("Enter a word to reverse: ");
-        String word = scanner.nextLine();
-
-        stringReverse(word);
+//        System.out.println("Enter a word to reverse: ");
+//        String string = scanner.nextLine();
+//        if(string.contains(" ")){
+//            stringReverse(string);
+//        }//else {
+////            stringReverse(string);
+////        }
     }
-    public static void stringReverse(String word){
-        for (int i = word.length()-1; i>=0;i--){
-            System.out.print(word.charAt(i));
+
+    public static void wordReverse(String string) {
+        for (int i = string.length() - 1; i >= 0; i--) {
+            System.out.print(string.charAt(i));
         }
+    }
+
+    public static void stringReverse(String string) {
+        String[] arr = string.split(" ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+//
+        }
+    }
+
+    public static void stringWordReverse(String string) {
+        char[] arr = string.toCharArray();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+//
+        }
+
     }
 }
