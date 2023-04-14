4cbe41ecc9f7f0323bd1867ca82b4628b2ba3fdc
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2023-04-14 10:09:14 +0100

    Calculator Programme that takes in an equation, splits the equation at the operand and returns the answer

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,37 +10,38 @@ public class Calculator {
         String equation = scanner.nextLine();
         solveEquation(equation);
     }
-    public static void solveEquation(String equation){
+
+    public static void solveEquation(String equation) {
         String[] arr;
         String operand = "";
         int num1;
         int num2;
 
-        if (equation.contains("+")){
+        if (equation.contains("+")) {
             arr = equation.split(Pattern.quote("+"));
             num1 = Integer.parseInt(arr[0]);
             num2 = Integer.parseInt(arr[1]);
             operand = "+";
-            System.out.println(num1 + operand + num2 + " = " +(num1+num2));
-        }else if (equation.contains("-")){
+            System.out.println(num1 + operand + num2 + " = " + (num1 + num2));
+        } else if (equation.contains("-")) {
             arr = equation.split(Pattern.quote("-"));
             num1 = Integer.parseInt(arr[0]);
             num2 = Integer.parseInt(arr[1]);
             operand = "-";
-            System.out.println(num1 + operand + num2 + " = " +(num1-num2));
-        }else if (equation.contains("*")){
+            System.out.println(num1 + operand + num2 + " = " + (num1 - num2));
+        } else if (equation.contains("*")) {
             arr = equation.split(Pattern.quote("*"));
             num1 = Integer.parseInt(arr[0]);
             num2 = Integer.parseInt(arr[1]);
             operand = "*";
-            System.out.println(num1 + operand + num2 + " = " +(num1*num2));
-        }else if (equation.contains("/")){
+            System.out.println(num1 + operand + num2 + " = " + (num1 * num2));
+        } else if (equation.contains("/")) {
             arr = equation.split(Pattern.quote("/"));
             num1 = Integer.parseInt(arr[0]);
             num2 = Integer.parseInt(arr[1]);
             operand = "/";
-            System.out.println(num1 + operand + num2 + " = " +(num1/num2));
-        }else {
+            System.out.println(num1 + operand + num2 + " = " + (num1 / num2));
+        } else {
             System.out.println("The equation you entered was not valid");
         }
 
