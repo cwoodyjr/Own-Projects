fcc6dd1509616d2b393f46d04a840ff7efe930dc
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2023-05-11 19:43:40 +0000

    Times Table programme to 12

---
+++ src/TimesTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TimesTable {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("What times table do you require?: ");
+        int timesTable = reader.nextInt();
+
+        for(int i = 0+1; i <= 12; i++){
+            System.out.println(i + " x " + timesTable + " = " + (i*timesTable));
+        }
+    }
+}
