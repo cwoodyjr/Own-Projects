d8e235b4f7f94c299de6f82aaef01f474dfc2e19
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2023-05-14 15:05:49 +0000

    added rand

--- src/JokeGenerator.java
+++ src/JokeGenerator.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class JokeGenerator {
@@ -7,7 +8,10 @@ public class JokeGenerator {
             String answer = reader.nextLine().toLowerCase();
 
             if(answer.toLowerCase().equals("y")){
-                tellJoke();
+                Random random = new Random();
+                int rand = random.nextInt(3)+1;
+                System.out.print(rand);
+                jokeA();
             }else if(answer.toLowerCase().equals("n")){
                 System.out.println("your BORING!!!");
             }else{
@@ -15,17 +19,17 @@ public class JokeGenerator {
             }
         }
     }
-    public static void tellJoke(){
-            try (Scanner reader = new Scanner(System.in)) {
-                System.out.println();System.out.print("knock knock...(press enter to continue)");
-                reader.nextLine();
-                System.out.print("who's there?...(press enter to continue)");
-                reader.nextLine();
-                System.out.print("Luke...(press enter to continue)");
-                reader.nextLine();
-                System.out.print("Luke who?...(press enter to contiue)");
-                reader.nextLine();
-                System.out.print("Luke through the spyhole and you'll see!! ");
-            }
+    public static void jokeA(){
+                try (Scanner reader = new Scanner(System.in)) {
+                    System.out.println();System.out.print("knock knock...(press enter to continue)");
+                    reader.nextLine();
+                    System.out.print("who's there?...(press enter to continue)");
+                    reader.nextLine();
+                    System.out.print("Luke...(press enter to continue)");
+                    reader.nextLine();
+                    System.out.print("Luke who?...(press enter to contiue)");
+                    reader.nextLine();
+                    System.out.print("Luke through the spyhole and you'll see!! ");
+                }
     }
 }
